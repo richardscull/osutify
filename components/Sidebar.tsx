003d6f99253c35f4aeadfa18ae1870d3f9f4087b
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-03-04 18:16:27 +0200

    fix: Use activeSong, not activeId

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -41,7 +41,7 @@ export function Sidebar({ children, packs }: SidebarProps) {
     <div
       className={twMerge(
         `flex h-full`,
-        player.activeId && "h-[calc(100%-80px)]"
+        player.activeSong && "h-[calc(100%-80px)]"
       )}
     >
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
